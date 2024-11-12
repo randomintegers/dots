b9c43fcf6bfe96b5355f358abb9462c7885b5879
randomintegers <raahimazfar@gmail.com>
2024-11-12 05:48:52 +0300

    Add files via upload

---
+++ nvim/lua/community.lua
@@ -0,0 +1,29 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  { -- further customize the options set by the community
+    "catppuccin",
+    opts = {
+      flavour = "auto", -- latte, frappe, macchiato, mocha
+      background = { -- :h background
+        light = "latte",
+        dark = "frappe",
+      },
+      integrations = {
+        sandwich = true,
+        noice = true,
+        mini = true,
+        leap = true,
+        markdown = true,
+        neotest = true,
+        cmp = true,
+        overseer = true,
+        lsp_trouble = true,
+        rainbow_delimiters = true,
+      },
+    },
+  },
+}
